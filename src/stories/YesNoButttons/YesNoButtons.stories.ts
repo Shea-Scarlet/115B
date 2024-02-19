6e45c6c9616d484ecb941af5a8052245b9c8418d
Toast Joe <josephascheidt@gmail.com>
2024-02-19 12:26:50 -0800

    Basic implementation of storybook description for Yes No Buttons

---
+++ src/stories/YesNoButttons/YesNoButtons.stories.ts
@@ -0,0 +1,26 @@
+import type { Meta, StoryObj } from '@storybook/angular';
+
+import { YesNoButtons } from './YesNoButtons';
+
+const meta: Meta<YesNoButtons> = {
+  title: 'Yes No Buttons',
+  component: YesNoButtons,
+  tags: ['autodocs'],
+  parameters: {
+    layout: 'fullscreen',
+    docs: {
+      description: {
+        // Add component description here:
+        component:
+          'The Yes No Buttons can be used as a selector for use in body boxes\
+          where the task can be optional saving the users choice as the buttons final state.',
+      },
+    },
+  },
+};
+
+export default meta;
+type Story = StoryObj<YesNoButtons>;
+
+// Exports story to storybook render
+export const Base: Story = {};
