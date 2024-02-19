1dd7b486716f0272b559e02f2b137a45dcc54695
Toast Joe <josephascheidt@gmail.com>
2024-02-19 11:46:35 -0800

    Basic implementation with description for Base Modal

--- src/stories/Base Modal/BaseModal.stories.ts
+++ src/stories/Base Modal/BaseModal.stories.ts
@@ -12,7 +12,11 @@ const meta: Meta<BaseModal> = {
       description: {
         // Add component description here:
         component:
-          'The Base Modal acts as a default frame for components that can be dynamically added as needed. The header contains three editable text boxes that changes based on tab drawer state or for use in pop up prompts. The body portion of the modal can contain body box components or cards when used as a pop up selection. The footer is available for button placement like next/previous or save and continue in cases where the modal is used as a pop up prompt. Every element can be dynamically added or forgone as needed by the engineer.',
+          'The Base Modal acts as a default frame for components that can be dynamically added as needed.\
+          The header contains three editable text boxes that changes based on tab drawer state or for use in pop up prompts.\
+          The body portion of the modal can contain body box components or cards when used as a pop up selection.\
+          The footer is available for button placement like next/previous or save and continue in cases where the modal is used as a pop up prompt.\
+          Every element can be dynamically added or forgone as needed by the engineer.',
       },
     },
   },
