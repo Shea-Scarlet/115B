1f271610bb673011ba3711561ca6c051f869e752
Toast Joe <josephascheidt@gmail.com>
2024-02-26 11:45:34 -0800

    Basic card implementation with angular material framework

--- src/stories/Card/Card.component.ts
+++ src/stories/Card/Card.component.ts
@@ -1,20 +1,37 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
-import type { User } from '../user';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'Card',
   standalone: true,
-  imports: [CommonModule],
-  template: `<article>
-    <section class="Card">
-      <h2>Card Component</h2>
-      <p>Card implementation tba.</p>
-    </section>
-  </article>`,
+  imports: [MatCardModule],
+  template: `
+  <mat-card class = "outter-card">
+    <div class = "card-content-container">
+      <div class = "photo-option">
+      <img mat-card-md-image src="https://material.angular.io/assets/img/examples/shiba2.jpg"  alt="Image of a Shiba Inu">
+      </div>
+      <div class = "text-options">
+        <div class = "title-option">
+        Welcome to example!
+        </div>
+        <div class = "explanation-option">
+        Im explaining the process of explaining! Woah, isnt that great!
+        </div>
+      </div>
+      <div class = "data-field-1">
+        pi = 89; 10 /= 31%
+      </div>
+      <div class = "data-field-2">
+        23/9 % 09 + m, i / 2
+      </div>
+      <div class = "checkbox-field">
+        check?
+      </div>
+    </div>
+  </mat-card>
+  `,
   styleUrls: ['./Card.css'],
 })
-export class Card {
-  user: User | null = null;
+export class CardComponent {
 }
