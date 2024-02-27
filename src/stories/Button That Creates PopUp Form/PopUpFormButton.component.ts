3010e0ed71b7905632f640c7a7981e86b11110f6
Valentin <vcasanov@ucsc.edu>
2024-02-27 13:04:50 -0800

    2.1.8

--- src/stories/Button That Creates PopUp Form/PopUpFormButton.component.ts
+++ src/stories/Button That Creates PopUp Form/PopUpFormButton.component.ts
@@ -1,18 +1,48 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input, Output, EventEmitter } from '@angular/core';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatCardModule} from '@angular/material/card';
 
 @Component({
   selector: 'PopUpForm-Button',
   standalone: true,
-  imports: [CommonModule],
-  template: ` <button
+  imports: [CommonModule, MatGridListModule, MatCardModule],
+  template: ` 
+<button
+    *ngIf="!popUpForm"
     type="button"
-    (click)="onClick.emit($event)"
+    (click)="onButtonClick()"
     [ngClass]="classes"
-    [ngStyle]="{ 'background-color': backgroundColor }"
-  >
+    >
     {{ label }}
-  </button>`,
+  </button>
+  <div *ngIf="popUpForm" [ngClass]="{'OuterBaseModal': true}"> 
+    <div [ngClass]="{'Header': true}"></div>
+      <div [ngClass]="{'Body': true}">
+        <mat-grid-list cols="1" rowHeight="100px">
+          <mat-grid-tile>
+            <mat-card>
+              <mat-card-content>
+              </mat-card-content>
+            </mat-card>
+          </mat-grid-tile>
+          <mat-grid-tile>
+            <mat-card></mat-card>
+          </mat-grid-tile>
+          <mat-grid-tile>
+            <mat-card></mat-card>
+          </mat-grid-tile>
+          <mat-grid-tile>
+            <mat-card></mat-card>
+          </mat-grid-tile>
+          <mat-grid-tile>
+            <mat-card></mat-card>
+          </mat-grid-tile>
+        </mat-grid-list>
+      </div>
+    <div [ngClass]="{'Footer': true}"> Footer</div>
+  </div>
+  `,
   styleUrls: ['./PopUpFormButton.css'],
 })
 export class PopUpFormButtonComponent {
@@ -48,6 +78,14 @@ export class PopUpFormButtonComponent {
   @Output()
   onClick = new EventEmitter<Event>();
 
+  popUpForm = false;
+  onButtonClick() {
+    this.popUpForm = true;
+    if (this.popUpForm){
+      this.onClick.emit();
+    }
+  }
+
   public get classes(): string[] {
     const mode = this.primary
       ? 'PopUpForm-Button--primary'
@@ -56,3 +94,4 @@ export class PopUpFormButtonComponent {
     return ['PopUpForm-Button', `PopUpForm-Button--${this.size}`, mode];
   }
 }
+
