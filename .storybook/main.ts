0249dbe7f045a70b05233e5a8ffc621a8eb52428
yuki-1184 <yuki.1184apple@gmail.com>
2024-02-27 15:24:48 -0800

    WIP not working

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,7 +1,7 @@
 import type { StorybookConfig } from "@storybook/angular";
 
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)", "../src"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
--- src/app/tab-drawer/tab-drawer.component.ts
+++ src/app/tab-drawer/tab-drawer.component.ts
@@ -2,11 +2,12 @@ import { Component, Input } from '@angular/core';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-tab-drawer',
   standalone: true,
-  imports: [MatTabsModule, MatButtonModule, MatIconModule],
+  imports: [MatTabsModule, MatButtonModule, MatIconModule, MatCardModule],
   templateUrl: './tab-drawer.component.html',
   styleUrl: './tab-drawer.component.css'
 })
