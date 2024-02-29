00d6824102e1e79b034917097f968c309ea385e9
Toa3t <josephascheidt@gmail.com>
2024-02-28 22:12:12 -0800

    Slow additions to CSS and general layout and functionality

--- src/stories/Base Modal/BaseModal.component.ts
+++ src/stories/Base Modal/BaseModal.component.ts
@@ -7,9 +7,14 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   template: `
-  <div class = modal-base>
-    <div class = "modal-header">
-      Im the header!
+  <div class = 'modal-base'>
+    <div class = 'modal-header'>
+      <p class = 'page-label-text'>SET UP GUIDE</p>
+      <h1 class = 'header-text'>Welcome Starbucks!</h1>
+      <p class = 'subtitle-text'> You're so awesome and you deserve at least two cookies.</p>
+    </div>
+    <div class = 'tab-drawer'>
+      Im tabs
     </div>
     <div class = "modal-body">
       Im the body!
