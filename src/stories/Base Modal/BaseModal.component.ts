f8c3862c0ff4e52461ab69404f00695491489698
Toast Joe <josephascheidt@gmail.com>
2024-03-02 10:57:22 -0800

    Added conditional containers for tab drawer and body boxes as well as better code base for future

--- src/stories/Base Modal/BaseModal.component.ts
+++ src/stories/Base Modal/BaseModal.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ContentChild, Input, ViewChild, TemplateRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 
@@ -7,26 +7,47 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   template: `
-  <div class = 'modal-base'>
-    <div class = 'modal-header'>
-      <p class = 'page-label-text'>SET UP GUIDE</p>
-      <h1 class = 'header-text'>Welcome Starbucks!</h1>
-      <p class = 'subtitle-text'> You're so awesome and you deserve at least two cookies.</p>
+  <div class="modal-base">
+      <div class="modal-header">
+        <p class="page-label-text" *ngIf="!hasCustomTitle">You're awesome!</p>
+        <h1 class="header-text" *ngIf="!hasCustomTitle">Welcome Home Depot!</h1>
+        <p class="subtitle-text" *ngIf="!hasCustomTitle">You're so awesome and you deserve at least two cookies!</p>
+      </div>
+
+      <div class="tab-drawer" *ngIf="tabDrawerContent">
+        <ng-container *ngTemplateOutlet="tabDrawerContent"></ng-container>
+      </div>
+
+      <!-- 
+        Example usage:
+        <BaseModal>
+          <div modal-body>
+            <app-card>Card Content 1</app-card>
+            <app-body-box>Body Box Content</app-body-box>
+            <app-card>Card Content 2</app-card>
+          </div>
+        </BaseModal
+      -->
+      <div class="modal-body">
+        <ng-content select="[modal-body]"></ng-content>
+        <div *ngIf="!hasCustomBody">I'm the default body!</div> 
+      </div>
+
+      <div class="modal-footer">
+        <ng-content select="[modal-footer]"></ng-content>
+        <div *ngIf="!hasCustomFooter">I'm the default footer!</div> 
+      </div>
     </div>
-    <div class = 'tab-drawer' [ngClass]="{'empty': !item.content}">
-      {{ item.content }}
-    </div>
-    <div class = "modal-body">
-      Im the body!
-    </div>
-    <div class = "modal-footer">
-      Im the footer!
-    </div>
-  </div>
   `,
   styleUrls: ['./BaseModal.css'],
 })
 export class BaseModalComponent {
-  item = { content: 'Tab drawer' };
-  
+  @Input() hasCustomTitle = false; 
+  @Input() hasCustomBody = false;
+  @Input() hasCustomFooter = false;
+
+  /*  Uses Angular's @ContentChild decorator to query for a TemplateRef with the 'tabDrawer' 
+      selector. This allows users to optionally slot in a template to represent the tab drawer's 
+      content. If no matching content is found, tabDrawerContent will be null.                  */
+  @ContentChild('tabDrawer') tabDrawerContent?: TemplateRef<any> | null;
 }
