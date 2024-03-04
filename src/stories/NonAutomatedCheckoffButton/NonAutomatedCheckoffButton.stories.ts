541d897695e7580117d1a456b6e2da35cf1ec9a6
Shea-Scarlet <gea.loro@gmail.com>
2024-03-03 23:54:13 -0800

    Finishing up the components

--- src/stories/NonAutomatedCheckoffButton/NonAutomatedCheckoffButton.stories.ts
+++ src/stories/NonAutomatedCheckoffButton/NonAutomatedCheckoffButton.stories.ts
@@ -2,25 +2,25 @@ import type { Meta, StoryObj } from '@storybook/angular';
 import { action } from '@storybook/addon-actions';
 import { NonAutomatedCheckoffButtonComponent } from './NonAutomatedCheckoffButton.component';
 
-export default {
-  title: 'NonAutomatedCheckoffButton',
+const meta: Meta<NonAutomatedCheckoffButtonComponent> = {
+  title: 'Non Automated Checkoff Button',
   component: NonAutomatedCheckoffButtonComponent,
-  argTypes: {
-    checkboxChange: { action: 'checkboxChange', table: { type: { summary: 'boolean' } } },
+  tags: ['autodocs'],
+  parameters: {
+    layout: 'fullscreen',
+    docs: {
+      description: {
+        // Add component description here:
+        component:
+          'The NonAutomatedCheckoffButton will be used to have the user select that\
+          they have correctly inputted the information before they continue to\
+          the next page.',
+      },
+    },
   },
-} as Meta;
-
-export const Uncheck: StoryObj<NonAutomatedCheckoffButtonComponent> = (args: NonAutomatedCheckoffButtonComponent) => ({
-  component: NonAutomatedCheckoffButtonComponent,
-  props: {
-    ...args,
-    checkboxChange: action('checkboxChange'),
-  },
-});
-Uncheck.args = {
-  user: null,
-  isChecked: false,
 };
+
+export default meta;
 
 export const Check: StoryObj<NonAutomatedCheckoffButtonComponent> = (args: NonAutomatedCheckoffButtonComponent) => ({
   component: NonAutomatedCheckoffButtonComponent,
@@ -32,4 +32,16 @@ export const Check: StoryObj<NonAutomatedCheckoffButtonComponent> = (args: NonAu
 Check.args = {
   user: null,
   isChecked: true,
+};
+
+export const Uncheck: StoryObj<NonAutomatedCheckoffButtonComponent> = (args: NonAutomatedCheckoffButtonComponent) => ({
+  component: NonAutomatedCheckoffButtonComponent,
+  props: {
+    ...args,
+    checkboxChange: action('checkboxChange'),
+  },
+});
+Uncheck.args = {
+  user: null,
+  isChecked: false,
 };
