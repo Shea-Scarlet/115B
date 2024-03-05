f8417fa9899bb20df5a6689e2e2dcd51babea56e
sshrest1582 <sshrest5@ucsc.edu>
2024-03-04 17:43:07 -0800

    completed buttons with links
    completed

--- src/stories/Buttons with Links/ButtonsWithLinks.component.ts
+++ src/stories/Buttons with Links/ButtonsWithLinks.component.ts
@@ -1,60 +1,32 @@
-import { CommonModule } from '@angular/common';
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
-  // eslint-disable-next-line @angular-eslint/component-selector
   selector: 'ButtonsWithLinks',
-  standalone: true,
-  imports: [CommonModule],
-  template: ` <button
-    type="button"
-    (click)="onClick.emit($event)"
-    [ngClass]="classes"
-    [ngStyle]="{ 'background-color': backgroundColor }"
-  >
-    {{ label }}
-  </button>`,
+  template: `
+    <button
+      [ngClass]="classes.join(' ')"
+      (click)="handleButtonClick()"
+    >
+      {{ label }}
+    </button>
+  `,
   styleUrls: ['./ButtonsWithLinks.css'],
 })
 export class ButtonsWithLinksComponent {
-  /**
-   * Is this the principal call to action on the page?
-   */
-  @Input()
-  primary = false;
-
-  /**
-   * What background color to use
-   */
-  @Input()
-  backgroundColor?: string;
-
-  /**
-   * How large should the button be?
-   */
-  @Input()
-  size: 'small' | 'medium' | 'large' = 'medium';
-
-  /**
-   * Button contents
-   *
-   * @required
-   */
-  @Input()
-  label = 'Button with Link';
-
-  /**
-   * Optional click handler
-   */
-  @Output()
-  // eslint-disable-next-line @angular-eslint/no-output-on-prefix
-  onClick = new EventEmitter<Event>();
+  @Input() link = ''; // Default hyperlink
+  @Input() primary = false;
+  @Input() backgroundColor?: string;
+  @Input() label = 'Button with no Link';
 
   public get classes(): string[] {
     const mode = this.primary
       ? 'ButtonsWithLinks--primary'
       : 'ButtonsWithLinks--secondary';
 
-    return ['ButtonsWithLinks', `ButtonsWithLinks--${this.size}`, mode];
+    return ['ButtonsWithLinks', mode];
+  }
+
+  handleButtonClick(): void {
+    window.open(this.link, '_blank');
   }
 }
